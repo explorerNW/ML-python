a73c3f0a42214e875f8027c01afc1019ec438609
NieWang <superexplorernw@163.com>
2024-09-25 15:33:15 +0800

    feat(001): scikit-learn

--- scikt-learn.py
+++ scikt-learn.py
@@ -63,4 +63,15 @@ lr = LinearRegression()
 result = cross_validate(lr, X, y)
 print('result = ', result)
 
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
+from sklearn.ensemble import RandomForestRegressor
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+param_distributions = { 'n_estimators': randint(1, 5), 'max_depth': randint(5, 10) }
+search = RandomizedSearchCV(estimator=RandomForestRegressor(random_state=0), n_iter=5, param_distributions=param_distributions, random_state=0)
+search.fit(X_train, y_train)
+print('search: ', search.best_params_)
+print('score: ', search.score(X_test, y_test))
+
+
 
